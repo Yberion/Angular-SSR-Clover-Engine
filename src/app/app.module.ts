a717604b8ec2447db35f5db568402f36c3ef1af8
Yberion <brandon.largeau@gmail.com>
2022-08-12 01:52:58 +0200

    feat(SSR): Add SSR using @nguniversal/common with Clover Engine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RendererModule, TransferHttpCacheModule } from '@nguniversal/common/clover';
+import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'appId' }),
+    AppRoutingModule,
+    RendererModule.forRoot(),
+    TransferHttpCacheModule
   ],
   providers: [],
   bootstrap: [AppComponent]
