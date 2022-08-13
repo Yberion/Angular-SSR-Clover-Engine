e42ba50fe692313bb64fbd4e27ff48b17444bd62
Yberion <brandon.largeau@gmail.com>
2022-08-14 01:34:54 +0200

    chore: rename project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular-SSR-Fastify'`, () => {
+  it(`should have as title 'Angular-SSR-Fastify-Clover'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-SSR-Fastify');
+    expect(app.title).toEqual('Angular-SSR-Fastify-Clover');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular-SSR-Fastify app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular-SSR-Fastify-Clover app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-SSR-Fastify';
+  title = 'Angular-SSR-Fastify-Clover';
 }
